ccbfb32e2be892edd35a47d506e1898bcbca82a2
Ali Rizvi <frontear4594@gmail.com>
2021-10-22 02:14:16 -0400

    draw a shape without a shader
    this fails on some systems, nw

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,33 @@ int main(int, char**) {
 
         glewExperimental = GL_TRUE;
         if (glewInit() == GLEW_OK) {
+            GLuint array[1];
+            glGenVertexArrays(1, array);
+            glBindVertexArray(array[0]);
+
+            GLfloat vertices[] = {
+                -1.0f, -1.0f, 0.0f,
+                 1.0f, -1.0f, 0.0f,
+                 0.0f,  1.0f, 0.0f
+            };
+
+            GLuint buffer[1];
+            glGenBuffers(1, buffer);
+            glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
             glfwSwapInterval(1);
             while (glfwWindowShouldClose(window) == GLFW_FALSE) {
                 glClear(GL_COLOR_BUFFER_BIT);
+
+                glEnableVertexAttribArray(0);
+
+                glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+                glDrawArrays(GL_TRIANGLES, 0, 3);
+
+                glDisableVertexAttribArray(0);
 
                 glfwSwapBuffers(window);
                 glfwPollEvents();
