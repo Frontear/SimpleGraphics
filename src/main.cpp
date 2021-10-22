b3ef8fc055cd2f1d8519541ce64101063a3f9a91
Ali Rizvi <frontear4594@gmail.com>
2021-10-22 11:43:01 -0400

    reduce unnecessary vao operation
    we can store it in our vao.

--- src/main.cpp
+++ src/main.cpp
@@ -64,20 +64,20 @@ int main() {
     glDetachShader(program, f_shader);
     glDeleteShader(f_shader);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
     while (x.isAlive()) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(program);
 
-        glEnableVertexAttribArray(0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, buffer[0]);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
         glDrawArrays(GL_TRIANGLES, 0, 3);
-
-        glDisableVertexAttribArray(0);
 
         x.update();
     }
+
+    glDeleteBuffers(1, buffer);
+    glDeleteVertexArrays(1, array);
+    glDeleteProgram(program);
 }
